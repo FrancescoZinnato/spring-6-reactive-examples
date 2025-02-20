4d4f4760f56810a953bb7e37c28c0808ef4dad64
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-20 16:50:14 +0100

    Flux test operations with examples of lambda/method reference ways to do it

--- src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -2,15 +2,17 @@ package guru.springframework.spring6reactiveexamples.repositories;
 
 import guru.springframework.spring6reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 class PersonRepositoryImplTest {
 
     PersonRepository repo = new PersonRepositoryImpl();
 
     @Test
-    void testMonoByIdBlock() { // Come NON fare un test con Mono/Flux
-
+    void testMonoByIdBlock() { // Come NON fare un test con Mono
         Mono<Person> personMono = repo.getById(1);
 
         Person person = personMono.block();
@@ -22,20 +24,56 @@ class PersonRepositoryImplTest {
 
     @Test
     void testGetByIdSubscriber() {
-
         Mono<Person> personMono = repo.getById(1);
 
         personMono.subscribe(System.out::println);
-
     }
 
     @Test
     void testMapOperation() {
-
         Mono<Person> personMono = repo.getById(1);
 
         personMono.map(Person::getFirstName).subscribe(System.out::println);
+    }
 
+    @Test
+    void testFluxBlockFirst() { // Come NON fare un test con Flux
+        Flux<Person> personFlux = repo.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person);
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = repo.findAll();
+
+        personFlux.subscribe(System.out::println);
+
+        personFlux.subscribe(person -> System.out.println(person));
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = repo.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(System.out::println);
+
+        personFlux.map(person -> person.getFirstName()).subscribe(firstName -> System.out.println(firstName));
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = repo.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(System.out::println);
+
+        listMono.subscribe(list -> list.forEach(System.out::println));
+
+        listMono.subscribe(list -> list.forEach(person -> System.out.println(person.getFirstName())));
     }
 
 }
