09f11b0f52a2727f2f0fb546748e1e2a04392b1a
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-20 17:34:38 +0100

    Error Handling in flux operations

--- src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -45,13 +45,24 @@ class PersonRepositoryImplTest {
         System.out.println(person);
     }
 
+    /*
+     La method reference (`System.out::println`) è utile quando vogliamo eseguire un'operazione direttamente sull'oggetto emesso dal Flux,
+     senza doverlo esplicitare con una lambda.
+
+     Ad esempio, `personFlux.subscribe(System.out::println)` è equivalente a `personFlux.subscribe(person -> System.out.println(person))`,
+     ma più conciso e leggibile.
+
+     Il metodo `println` di `System.out` accetta un oggetto e chiama automaticamente il suo `toString()`, quindi possiamo usare
+     direttamente `System.out::println` invece di scrivere una lambda esplicita.
+    */
+
     @Test
     void testFluxSubscriber() {
         Flux<Person> personFlux = repo.findAll();
 
         personFlux.subscribe(System.out::println);
 
-        personFlux.subscribe(person -> System.out.println(person));
+        personFlux.subscribe(person -> System.out.println(person.toString()));
     }
 
     @Test
@@ -86,6 +97,48 @@ class PersonRepositoryImplTest {
         Mono<Person> personMono = repo.findAll().filter(person -> person.getFirstName().equals("Fiona")).next();
 
         personMono.subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testFindPersonByIdNotFound() {
+        Flux<Person> personFlux = repo.findAll();
+
+        final Integer id = 8; // Non possiamo mutare variabili in un reactive stream, quindi le variabili saranno definite final!
+
+        /*
+        Il metodo .next() emette solo il primo valore emesso dal Flux in un nuovo Mono. Se è chiamato su un Flux vuoto, emette un Mono vuoto.
+        Utilizziamo quindi .single(), che si aspetta ed emette un singolo valore dal Flux,
+        oppure una NoSuchElementException per un Flux vuoto, oppure IndexOutOfBoundsException per una source con più di un elemento!
+         */
+        Mono<Person> personMono = personFlux.filter(person -> person.getId().equals(id)).single()
+                .doOnError(throwable -> {
+                    System.out.println("Error occurred in the flux");
+                    System.out.println(throwable.toString());
+                });
+
+        /*
+         Se rimuoviamo completamente il blocco `.subscribe()`, nessun errore verrà emesso, né il Mono verrà eseguito.
+
+         Questo accade perché Project Reactor segue un modello lazy (a esecuzione pigra):
+         un flusso reattivo (Flux/Mono) non viene eseguito fino a quando non c'è un subscriber.
+
+         In questo caso, `personMono` è definito, ma non essendoci nessun `.subscribe()`, il flusso non viene attivato
+         e quindi nessuna operazione viene effettivamente eseguita.
+
+         Di conseguenza:
+         - Il filtro `.filter(person -> person.getId().equals(id))` non viene applicato.
+         - Il `.single()` non viene valutato, quindi non viene mai generata un'eccezione (NoSuchElementException in caso di Mono vuoto).
+         - Il `.doOnError(...)` non viene mai eseguito.
+
+         Senza la subscribe, tutto rimane dichiarativo, ma non viene mai eseguito.
+        */
+        personMono.subscribe(person -> { // La statement lambda può essere sostituita con expression lambda (no bracket, stessa riga).
+                                                // Tutta la lambda può essere sostituita da "System.out::println", essendo che stiamo effettuando il print dell'oggetto risultante dal subscribe
+            System.out.println(person.toString());
+        }, throwable -> {
+            System.out.println("Error occurred in the mono");
+            System.out.println(throwable.toString());
+        });
     }
 
 }
